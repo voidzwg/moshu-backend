719d1ecbec38ddef0eed1be741e80dfcf14fbcdb
Zhuang Weiguang <1454101799@qq.com>
2022-08-02 19:00:31 +0800

    添加接口

--- com/funcs.py
+++ com/funcs.py
@@ -1,1 +1,36 @@
 from django.http import JsonResponse
+import re
+
+
+def check_email(email):
+    if email == "":
+        return True
+    ex_email = re.compile(r'^[\w]+@[\w.]+com|net|cn')
+    result = ex_email.match(email)
+    if result:
+        return True
+    return False
+
+
+# 必须包含字母和数字，且长度在8和18之间，否则为非法，返回False
+def check_password(password):
+    check_alpha = True
+    check_digit = True
+    check_len = True
+    length = 0
+    for ch in password:
+        length += 1
+        if '0' <= ch <= '9' or 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+            if '0' <= ch <= '9':
+                check_digit = False
+            if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+                check_alpha = False
+        else:
+            check_alpha = check_digit = True
+            break
+    if 8 <= length <= 18:
+        check_len = False
+    if check_alpha or check_digit or check_len:
+        return False
+    return True
+

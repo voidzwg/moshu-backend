f94954671440057231321596bc7b17263933241b
Zhuang Weiguang <1454101799@qq.com>
2022-08-02 10:27:04 +0800

    初始化login接口

---
+++ login/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from com.funcs import *
+
+
+# Author: zwg
+# Create your views here.
+@csrf_exempt  # 跨域设置
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username is None or password is None:
+            return JsonResponse({'errno': 1, 'msg': "请输入用户名和密码"})
+        pass
+    return JsonResponse({'errno': 1, 'msg': "请求方式错误"})
+

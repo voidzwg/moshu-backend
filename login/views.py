c6265f4fc0da73663ba1a5fa878749905d0242c2
Zhuang Weiguang <1454101799@qq.com>
2022-08-04 22:15:24 +0800

    debug:任何注册用户都会被提示用户名已被占用

--- login/views.py
+++ login/views.py
@@ -33,7 +33,7 @@ def register(request):
         if username is None or password_1 is None:
             return JsonResponse({'errno': 2, 'msg': "请输入用户名和密码"})
         test_user = Users.objects.filter(username=username)
-        if test_user is not None:
+        if not test_user:
             return JsonResponse({'errno': 2, 'msg': "用户名已被占用"})
         if not check_password(password_1):
             return JsonResponse({'errno': 2, 'msg': "密码格式错误"})

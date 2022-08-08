fe5769159e11e9ce74ca2102579e93bd5b953018
Zhuang Weiguang <1454101799@qq.com>
2022-08-08 12:50:07 +0800

    修改路径

--- moshu/settings.py
+++ moshu/settings.py
@@ -133,7 +133,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

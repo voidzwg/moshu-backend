167928a220dde9376e0115f866741ccdf7c20ac8
voidzwg <1454101799@qq.com>
2022-08-09 22:50:26 +0800

    修复无法创建项目的bug******

--- project_manage/models.py
+++ project_manage/models.py
@@ -18,6 +18,121 @@ class Groups(models.Model):
         managed = False
         db_table = '_groups'
 
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
 class Document(models.Model):
     pid = models.ForeignKey('Projects', models.DO_NOTHING, db_column='pid')
     data = models.TextField()
@@ -69,7 +184,7 @@ class Projects(models.Model):
 
 class Prototype(models.Model):
     pid = models.ForeignKey(Projects, models.DO_NOTHING, db_column='pid')
-    data = models.TextField()
+    data = models.CharField(max_length=255)
     name = models.CharField(max_length=100)
     width = models.IntegerField()
     height = models.IntegerField()
@@ -81,6 +196,7 @@ class Prototype(models.Model):
         managed = False
         db_table = 'prototype'
 
+
 class Template(models.Model):
     name = models.CharField(max_length=100)
     data = models.TextField()
@@ -90,6 +206,7 @@ class Template(models.Model):
     class Meta:
         managed = False
         db_table = 'template'
+
 
 class Uml(models.Model):
     pid = models.ForeignKey(Projects, models.DO_NOTHING, db_column='pid')
@@ -107,7 +224,7 @@ class Uml(models.Model):
 class Users(models.Model):
     username = models.CharField(max_length=18)
     field_password = models.CharField(db_column='_password', max_length=18)  # Field renamed because it started with '_'.
-    avatar = models.ImageField(upload_to='avatars/', max_length=255)
+    avatar = models.CharField(max_length=255)
     name = models.CharField(max_length=18)
     email = models.CharField(max_length=18)
     gnum = models.IntegerField()

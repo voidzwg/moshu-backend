32dffb7f6d34e19966ab1a2d675fdcefd0beccb8
voidzwg <1454101799@qq.com>
2022-08-09 10:51:29 +0800

    去除MEDIA_URL和STATIC_URL前面的'/'

--- moshu/settings.py
+++ moshu/settings.py
@@ -131,9 +131,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field

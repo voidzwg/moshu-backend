62a5de62e57e29149c3e05b67df7bde96f475dbb
DsLipku <1481304174@qq.com>
2022-08-10 19:19:31 +0800

    完善文件系统（2）

--- documents_center/urls.py
+++ documents_center/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('',documents_center),
+    path('open_file/',open_file),
+    path('create_file/',create_file),
 ]
--- documents_center/views.py
+++ documents_center/views.py
@@ -16,16 +16,18 @@ def documents_center(request):
         try:
             next_list = root.get_children()
             data = []
-            list = {
-                'Project':'项目文档区',
-                'Data':'数据',
-                'Others':'其他文件夹',
-            }
             for node in next_list:
                 name = node.name.split('_')[1]
+                if name == 'Project':
+                    name = '项目文档区'
+                elif name == 'Data':
+                    name = '数据'
+                elif name == 'Others':
+                    name = '其他文件夹'
                 tmp = {
                     'id':node.id,
-                    'name':list[name],
+                    'name':name,
+                    'type':node.isfile,
                 }
                 data.append(tmp)
         except Exception as e:
@@ -37,15 +39,15 @@ def documents_center(request):
 
 def open_file(request):
     if request.method == 'POST':
-        id = request.GET.get('id')
+        id = request.POST.get('id')
         if id is None:
             return JsonResponse({'errno': 1002, 'msg': "参数为空"})
         try:
-            file = Files.objects.get(name=id)
+            file = Files.objects.get(id=id)
         except Exception as e:
             print(e)
             return JsonResponse({'errno': 1003, 'msg': "找不到该文件或目录！"})
-        if file.isFile:
+        if file.isfile:
             return open_document(request)
         try:
             next_list = file.get_children()
@@ -53,12 +55,40 @@ def open_file(request):
             for node in next_list:
                 if file.name.split('_')[1] == 'Project':
                     name = node.name.split('_')[2*node.level-1]
+                else:
+                    name = node.name.split('_')[node.level]
                 tmp = {
                     'id': node.id,
-                    'name': node.name,
+                    'name': name,
+                    'type':node.isfile,
                 }
                 data.append(tmp)
         except Exception as e:
-            print()
+            print(e)
+            return JsonResponse({'errno': 1004, 'msg': "未知错误！"})
+        return JsonResponse(data,safe=False)
+    else:
+        return JsonResponse({'errno': 1001, 'msg': "请求方式错误"})
+
+def create_file(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        name =request.POST.get('name')
+        type = request.POST.get('type')
+        if id is None:
+            return JsonResponse({'errno': 1002, 'msg': "参数为空"})
+        try:
+            file = Files.objects.get(id=id)
+        except Exception as e:
+            print(e)
+            return JsonResponse({'errno': 1003, 'msg': "找不到该目录！"})
+        try:
+            name = file.name + '_' + name
+            newFile = Files(name=name,parent=file,isfile=type)
+            newFile.save()
+        except Exception as e:
+            print(e)
+            return JsonResponse({'errno': 1004, 'msg': "未知错误！"})
+        return JsonResponse({'errno': 0, 'msg': "新建成功"})
     else:
         return JsonResponse({'errno': 1001, 'msg': "请求方式错误"})

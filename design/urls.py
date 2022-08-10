97be3a331af2130131ddfd72988e2003221abdea
Zhuang Weiguang <1454101799@qq.com>
2022-08-10 23:24:04 +0800

    新增预览相关模块

--- design/urls.py
+++ design/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('get_design/', get_design),
     path('get_one_design/', get_one_design),
     path('search_design/', search_design),
-    path('get_templates/',get_templates),
-    path('open_template/',open_template),
+    path('get_templates/', get_templates),
+    path('open_template/', open_template),
+    path('upload_prototype/', upload_prototype),
+    path('get_prototype_img/', get_prototype_img),
+    path('get_show_status/', get_show_status),
+    path('change_show_status/', change_show_status),
 ]
